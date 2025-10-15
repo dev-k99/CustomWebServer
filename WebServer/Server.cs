9b94063bdae32017f2151205739bbb58a36d5a7f
dev-k99 <kwanelerh069@gmail.com>
2025-10-15 11:19:42 +0200

    Initial Commit: Set up CustomWebServern library and WebServerApp console app with HttpListener

---
+++ WebServer/Server.cs
@@ -0,0 +1,96 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace WebServer
+
+  {
+    public static class Server
+    {
+        private static HttpListener listener;
+
+        /// <summary>
+        /// Returns list of IP addresses assigned to localhost network devices, such as hardwired ethernet, wireless, etc.
+        /// </summary>
+        private static List<IPAddress> GetLocalHostIPs()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            List<IPAddress> ret = host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
+
+            return ret;
+        }
+
+        //Instantiate the HttpListener and add the localhost prefixes:
+        private static HttpListener InitializeListener(List<IPAddress> localhostIPs)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost/");
+
+            // Listen to IP address as well.
+            localhostIPs.ForEach(ip =>
+            {
+                Console.WriteLine("Listening on IP " + "http://" + ip.ToString() + "/");
+                listener.Prefixes.Add("http://" + ip.ToString() + "/");
+            });
+
+            return listener;
+        }
+
+        public static int maxSimultaneousConnections = 20;
+        private static Semaphore sem = new Semaphore(maxSimultaneousConnections, maxSimultaneousConnections);
+
+        /// <summary>
+        /// Begin listening to connections on a separate worker thread.
+        /// </summary>
+        private static void Start(HttpListener listener)
+        {
+            listener.Start();
+            Task.Run(() => RunServer(listener));
+        }
+
+        /// <summary>
+        /// Start awaiting for connections, up to the "maxSimultaneousConnections" value.
+        /// This code runs in a separate thread.
+        /// </summary>
+        private static void RunServer(HttpListener listener)
+        {
+            while (true)
+            {
+                sem.WaitOne();
+                StartConnectionListener(listener);
+            }
+        }
+
+        /// <summary>
+        /// Await connections.
+        /// </summary>
+        private static async void StartConnectionListener(HttpListener listener)
+        {
+            // Wait for a connection. Return to caller while we wait.
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            // Release the semaphore so that another listener can be immediately started up.
+            sem.Release();
+
+            // We have a connection, do something...
+            string response = "Hello Browser!";
+            byte[] encoded = Encoding.UTF8.GetBytes(response);
+            context.Response.ContentLength64 = encoded.Length;
+            context.Response.OutputStream.Write(encoded, 0, encoded.Length);
+            context.Response.OutputStream.Close();
+        }
+
+        /// <summary>
+        /// Starts the web server.
+        /// </summary>
+        public static void Start()
+        {
+            List<IPAddress> localHostIPs = GetLocalHostIPs();
+            HttpListener listener = InitializeListener(localHostIPs);
+            Start(listener);
+        }
+
+    }
+}
---
+++ WebServerApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServer;
+
+namespace WebServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Start();
+            Console.ReadLine();
+        }
+    }
+}
